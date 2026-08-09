84d78d70fd66d353a07cc4fc2816c6a50046ae5c
Iboy-xnetwork <deandie17@gmail.com>
2026-08-09 13:19:38 +0200

    Update server port for deployment

--- backend/index.js
+++ backend/index.js
@@ -232,8 +232,8 @@ app.delete("/api/brews/:id", async (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
